8ba624317bcca9a74f58baf8fe9d9f6346bdbf14
hakelimopu <hakelimopu@gmail.com>
2016-02-04 06:58:56 -0600

    render spaces

--- Project2/Project2/SDL/SDLRender.fs
+++ Project2/Project2/SDL/SDLRender.fs
@@ -46,13 +46,13 @@ module private SDLRenderNative =
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
     extern int SDL_CreateWindowAndRenderer(int width, int height, uint32 window_flags, IntPtr window, IntPtr renderer)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern Renderer  SDL_CreateSoftwareRenderer(Surface surface)
+    extern Renderer SDL_CreateSoftwareRenderer(Surface surface)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern Renderer  SDL_GetRenderer(IntPtr  window)
+    extern Renderer SDL_GetRenderer(IntPtr  window)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_GetRendererInfo(Renderer  renderer, IntPtr info)
+    extern int SDL_GetRendererInfo(Renderer renderer, IntPtr info)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_GetRendererOutputSize(Renderer  renderer, IntPtr w, IntPtr h)
+    extern int SDL_GetRendererOutputSize(Renderer renderer, IntPtr w, IntPtr h)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
     extern int SDL_RenderTargetSupported(Renderer renderer)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
@@ -60,45 +60,45 @@ module private SDLRenderNative =
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
     extern Texture  SDL_GetRenderTarget(Renderer renderer)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern void SDL_RenderGetLogicalSize(Renderer  renderer, IntPtr w, IntPtr h)
+    extern void SDL_RenderGetLogicalSize(Renderer renderer, IntPtr w, IntPtr h)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderSetViewport(Renderer  renderer, IntPtr rect)
+    extern int SDL_RenderSetViewport(Renderer renderer, IntPtr rect)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern void SDL_RenderGetViewport(Renderer  renderer, IntPtr rect)
+    extern void SDL_RenderGetViewport(Renderer renderer, IntPtr rect)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderSetClipRect(Renderer  renderer, IntPtr rect)
+    extern int SDL_RenderSetClipRect(Renderer renderer, IntPtr rect)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern void SDL_RenderGetClipRect(Renderer  renderer, IntPtr rect)
+    extern void SDL_RenderGetClipRect(Renderer renderer, IntPtr rect)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderIsClipEnabled(Renderer  renderer)
+    extern int SDL_RenderIsClipEnabled(Renderer renderer)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
     extern int SDL_RenderSetScale(Renderer  renderer, float scaleX, float scaleY)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern void SDL_RenderGetScale(Renderer  renderer, IntPtr scaleX, IntPtr scaleY)
+    extern void SDL_RenderGetScale(Renderer renderer, IntPtr scaleX, IntPtr scaleY)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_GetRenderDrawColor(Renderer  renderer, IntPtr  r, IntPtr  g, IntPtr  b, IntPtr  a)
+    extern int SDL_GetRenderDrawColor(Renderer renderer, IntPtr  r, IntPtr  g, IntPtr  b, IntPtr  a)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_SetRenderDrawBlendMode(Renderer  renderer, int blendMode)
+    extern int SDL_SetRenderDrawBlendMode(Renderer renderer, int blendMode)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_GetRenderDrawBlendMode(Renderer  renderer, IntPtr blendMode)
+    extern int SDL_GetRenderDrawBlendMode(Renderer renderer, IntPtr blendMode)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderDrawPoint(Renderer  renderer, int x, int y)
+    extern int SDL_RenderDrawPoint(Renderer renderer, int x, int y)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderDrawPoints(Renderer  renderer, IntPtr   points, int count)
+    extern int SDL_RenderDrawPoints(Renderer renderer, IntPtr   points, int count)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderDrawLine(Renderer  renderer, int x1, int y1, int x2, int y2)
+    extern int SDL_RenderDrawLine(Renderer renderer, int x1, int y1, int x2, int y2)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderDrawLines(Renderer  renderer, IntPtr   points, int count)
+    extern int SDL_RenderDrawLines(Renderer renderer, IntPtr   points, int count)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderDrawRect(Renderer  renderer, IntPtr rect)
+    extern int SDL_RenderDrawRect(Renderer renderer, IntPtr rect)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderDrawRects(Renderer  renderer, IntPtr rects, int count)
+    extern int SDL_RenderDrawRects(Renderer renderer, IntPtr rects, int count)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderFillRect(Renderer  renderer, IntPtr rect)
+    extern int SDL_RenderFillRect(Renderer renderer, IntPtr rect)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderFillRects(Renderer  renderer, IntPtr rects, int count)
+    extern int SDL_RenderFillRects(Renderer renderer, IntPtr rects, int count)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
-    extern int SDL_RenderCopyEx(Renderer  renderer, Texture  texture, IntPtr srcrect, IntPtr dstrect, double angle, IntPtr  center, int flip)
+    extern int SDL_RenderCopyEx(Renderer renderer, Texture  texture, IntPtr srcrect, IntPtr dstrect, double angle, IntPtr  center, int flip)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
     extern int SDL_RenderReadPixels(Renderer  renderer,IntPtr rect,uint32 format,IntPtr pixels, int pitch)
     [<DllImport(@"SDL2.dll", CallingConvention = CallingConvention.Cdecl)>]
