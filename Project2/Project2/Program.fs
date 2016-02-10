46d334fd15511e35553c5d764893d0d2ff4c66ee
hakelimopu <hakelimopu@gmail.com>
2016-02-10 08:45:39 -0600

    screw alpha

--- Project2/Project2/Program.fs
+++ Project2/Project2/Program.fs
@@ -13,21 +13,21 @@ let main argv =
 
     use mainRenderer = SDLRender.create mainWindow -1 SDLRender.Flags.Accelerated
 
-    use surface = SDLSurface.createRGB (320<px>,240<px>,32<bit/px>) (0x00FF0000u,0x0000FF00u,0x000000FFu,0xFF000000u)
+    use surface = SDLSurface.createRGB (320<px>,240<px>,32<bit/px>) (0x00FF0000u,0x0000FF00u,0x000000FFu,0x00000000u)
 
-    use workSurface = SDLSurface.createRGB (8<px>,8<px>,32<bit/px>) (0x00FF0000u,0x0000FF00u,0x000000FFu,0xFF000000u)
+    use workSurface = SDLSurface.createRGB (8<px>,8<px>,32<bit/px>) (0x00FF0000u,0x0000FF00u,0x000000FFu,0x00000000u)
 
     workSurface
     |> SDLSurface.setColorKey (Some {Red=0uy;Green=0uy;Blue=0uy;Alpha=0uy})
     |> ignore
 
-    use bitmap = SDLSurface.loadBmp SDLPixel.ARGB8888Format "Content/romfont8x8.bmp"
+    use bitmap = SDLSurface.loadBmp SDLPixel.RGB888Format "Content/romfont8x8.bmp"
 
     bitmap
     |> SDLSurface.setColorKey (Some {Red=255uy;Green=255uy;Blue=255uy;Alpha=0uy})
     |> ignore
 
-    use mainTexture = mainRenderer |> SDLTexture.create SDLPixel.ARGB8888Format SDLTexture.Access.Streaming (320<px>,240<px>)
+    use mainTexture = mainRenderer |> SDLTexture.create SDLPixel.RGB888Format SDLTexture.Access.Streaming (320<px>,240<px>)
 
     mainRenderer |> SDLRender.setLogicalSize (320<px>,240<px>) |> ignore
 
